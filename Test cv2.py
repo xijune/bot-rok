599ef5dbea524b70f748cd2237eaaa09195b3a09
XiJune <stefan.petrovic31@hotmail.fr>
2021-06-23 15:43:37 +0200

    Méthode Finale
    Detection d'image dans screenshot + clique à la position trouvé

--- Test cv2.py
+++ Test cv2.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 import numpy as np
-import re
+import win32gui
+from PIL import Image, ImageGrab
 import time
-from PIL import Image
-import os
+import re
 from ppadb.client import Client
+
+# Addition pour cliquer à la bonne place
+RESX = 195
+RESY = 180
 
 def Adb():
     # Connection sur l'emulateur avec l'ip et port de base
@@ -16,46 +20,68 @@ def Adb():
     device = devices[0]
     return device
 
-def ScreenShot():
-    #Prend un screenshot
-    image = Adb().screencap()
-    with open('1.png', 'wb') as f:
-        f.write(image)
-    image = '1.png'
-    return image
 
+def ImgRecherche(template):
+    target = cv.imread('screenshot.png')
+    template = cv.imread('images/' + template)
 
-def template_demo():
-    screen = ScreenShot()
-    capture = 'archer.png'
+    time.sleep(1)
+    md = cv.TM_CCOEFF_NORMED
+    th, tw = template.shape[:2]
+    centers = []
 
-    target = cv.imread(screen, cv.IMREAD_UNCHANGED)
-    template = cv.imread(capture, cv.IMREAD_UNCHANGED)
+    result = cv.matchTemplate(target, template, md)
 
-    result = cv.matchTemplate(target, template, cv.TM_CCOEFF_NORMED)
+    threshold = 0.469
 
-    min_val, max_val, max_loc, min_loc = cv.minMaxLoc(result)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    tl = max_loc
+    br = (tl[0]+tw, tl[1]+th)
+    centers.append((max_loc[0] + tw//2 + RESX, max_loc[1] + th//2 + RESY))
 
-    print('Best match top left position: %s' % str(max_loc))
-    print('Best match confidence: %s' % str(max_val))
+    centers = re.sub("[^0-9]", " ", str(centers))
 
-    threshold = 0.99
-    if max_val >= threshold:
-        print('Found archer')
+    print(max_val)
+    if max_val > threshold:
+        print(centers)
+        cv.rectangle(target, tl, br, (0,0,255))
+        cv.imshow('matches', target)
+        cv.waitKey(0)
 
-        archer_w = template.shape[1]
-        archer_h = template.shape[0]
+        return centers
+    if not tl[0] in range(10, 40) or tl[1] > 20:
+        return False
 
-        top_left = max_loc
-        bottom_right = (top_left[0] + archer_w, top_left[1] + archer_h)
-
-        cv.rectangle(target, top_left, bottom_right, color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
     
-        cv.imshow('Result', target)
-        cv.waitKey()
 
-    else:
-        print('Archer not found')
+def screenshot():
+    windows_list = []
+    toplist = []
+    def enum_win(hwnd, result):
+        win_text = win32gui.GetWindowText(hwnd)
+        windows_list.append((hwnd, win_text))
+    win32gui.EnumWindows(enum_win, toplist)
 
+    game_hwnd = 0
+    for (hwnd, win_text) in windows_list:
+        if "BlueStacks" in win_text:
+            game_hwnd = hwnd
 
-template_demo()
+    win32gui.ShowWindow(game_hwnd, 9) # Un-minimize
+    win32gui.ShowWindow(game_hwnd, 5)
+    win32gui.SetForegroundWindow(game_hwnd)
+    time.sleep(1)
+
+    position = win32gui.GetWindowRect(game_hwnd)
+
+    position = (position[0], position[1] + 33, position[2] - 33, position[3])
+
+    screenshot = ImageGrab.grab(position)
+    screenshot = np.array(screenshot)
+    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+    cv.imwrite('screenshot.png', screenshot)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+screenshot()
+ImgRecherche('mais.png')
--- riseOfKingdom.py
+++ riseOfKingdom.py
@@ -1,46 +1,17 @@
 from ppadb.client import Client
-from PIL import Image
+from PIL import ImageGrab
 import time
-import tkinter as tk
 import cv2 as cv
 import numpy as np
 import re
+import win32gui
 
+# Addition pour cliquer à la bonne place
+RESX = 195
+RESY = 180
 
-SENSI =  0.949
-
-TRAPIDE = 1
-TLENT = 3
-
-BOUTTONHOME = (113, 229, 252, 255)
-
-EXPLORERJOURMIN = (110, 114, 87, 255)
-EXPLORERJOURMAX = (130, 134, 107, 255)
-EXPLORERNUITMIN = (80, 93, 88, 255)
-EXPLORERNUITMAX = (100, 113, 108, 255)
-
-FORMATIONJOURMIN = (137, 163, 68, 255)
-FORMATIONJOURMAX = (157, 183, 88, 255)
-
-IFORMATIONNUITMIN = (36, 85, 74, 255)
-IFORMATIONNUITMAX = (56, 105, 94, 255)
-
-AFORMATIONNUITMIN = (37, 85, 79, 255)
-AFORMATIONNUITMAX = (57, 105, 99, 255)
-
-PRETMIN = (148, 60, 0, 255)
-PRETMAX = (168, 80, 20, 255)
-
-CADEAUMIN = (217, 0, 0, 255)
-CADEAUMAX = (237, 20, 20, 255)
-
-RECLAMERMIN = (0, 173, 4, 255)
-RECLAMERMAX = (20, 193, 24, 255)
-
-NOTIFMIN = (217, 0, 0, 255)
-NOTIFMAX = (237, 20, 20, 255)
-
-bolReclame = False
+TRAPIDE = 0.5
+TLENT = 4
 
 def Adb():
     # Connection sur l'emulateur avec l'ip et port de base
@@ -52,57 +23,70 @@ def Adb():
     device = devices[0]
     return device
 
+
 def Tap(cordsTap):
     #Tap a la coordonnee choisi
     tap = Adb().shell('input touchscreen tap ' + cordsTap)
     return tap
 
-def ScreenShot():
-    #Prend un screenshot et lis l'image
-    image = Adb().screencap()
-    with open('screen.png', 'wb') as f:
-        f.write(image)
-    image = cv.imread('screen.png')
-    return image
 
-def ImgRecherche(template):
-    target = ScreenShot()
-    template = cv.imread(template)
-    time.sleep(TRAPIDE)
-    method = [cv.TM_SQDIFF_NORMED, cv.TM_CCORR_NORMED]
+def ScreenShot():
+    windows_list = []
+    toplist = []
+    def enum_win(hwnd, result):
+        win_text = win32gui.GetWindowText(hwnd)
+        windows_list.append((hwnd, win_text))
+    win32gui.EnumWindows(enum_win, toplist)
+
+    game_hwnd = 0
+    for (hwnd, win_text) in windows_list:
+        if "BlueStacks" in win_text:
+            game_hwnd = hwnd
+
+    win32gui.ShowWindow(game_hwnd, 9) # Un-minimize
+    win32gui.ShowWindow(game_hwnd, 5)
+    win32gui.SetForegroundWindow(game_hwnd)
+    time.sleep(1)
+
+    position = win32gui.GetWindowRect(game_hwnd)
+
+    position = (position[0], position[1] + 33, position[2] - 33, position[3])
+
+    screenshot = ImageGrab.grab(position)
+    screenshot = np.array(screenshot)
+    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+    cv.imwrite('screenshot.png', screenshot)
+
+
+def ImgRecherche(template, threshold):
+    ScreenShot()
+    target = cv.imread('screenshot.png')
+    template = cv.imread('images/' + template)
+
+    time.sleep(1)
+    md = cv.TM_CCOEFF_NORMED
     th, tw = template.shape[:2]
     centers = []
-    for md in method:
-        result = cv.matchTemplate(target, template, md)
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-        if md == cv.TM_SQDIFF_NORMED:
-            tl = min_loc
-        else:
-            tl = max_loc
-        br = (tl[0]+tw, tl[1]+th)
-        cv.rectangle(target, tl, br, (0,0,255))
 
-    centers.append((max_loc[0] + tw//2, max_loc[1] + th//2))
-
-    cv.imwrite('output.png', target)
+    result = cv.matchTemplate(target, template, md)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    tl = max_loc
+    br = (tl[0]+tw, tl[1]+th)
+    centers.append((max_loc[0] + tw//2 + RESX, max_loc[1] + th//2 + RESY))
 
     centers = re.sub("[^0-9]", " ", str(centers))
+    
+    print(max_val)
+    cv.rectangle(target, tl, br, (0,0,255))
+    cv.imshow('matches', target)
+    cv.waitKey(0)
+    if max_val > threshold:
+        print(centers)
+        return centers
 
-    return centers
+    if not tl[0] in range(10, 40) or tl[1] > 20:
+        return ''
 
-def Pix(cordPixX, cordPixY):
-    #Prend le RGBA du pixel de la coordonée
-    pix = ScreenShot().getpixel((cordPixX, cordPixY))
-    return pix
-
-def Base():
-    #Si correcte, appuis sur le boutton Home
-    pix = Pix(76, 981)
-    if pix == BOUTTONHOME:
-        Tap('76 981')
-        time.sleep(TLENT)
-    else:
-        pass
 
 def Exploration():
     Tap(ImgRecherche('explorer.png'))
@@ -111,6 +95,9 @@ def Exploration():
     time.sleep(TRAPIDE)
     Tap(ImgRecherche('btnExplorer.png'))
     time.sleep(TLENT)
+    Tap(ImgRecherche('btnExplorer.png'))
+    time.sleep(TRAPIDE)
+    Tap(ImgRecherche('btnExplorer.png'))
 
 
 def Collecte():
@@ -120,62 +107,22 @@ def Collecte():
 
     print('Collection terminee')
 
+
 def Infenterie():
-    #Si correcte, passe
-    pix = Pix(695, 741)
-    if pix <= FORMATIONJOURMAX and pix >= FORMATIONJOURMIN or pix <= IFORMATIONNUITMAX and pix >= IFORMATIONNUITMIN:
-        print('Lancement infenterie')
+    ll = 2
 
-        pix = Pix(770, 565)
-        if pix <= PRETMAX and pix >= PRETMIN:
-            Tap('780 650')
-            time.sleep(TLENT)
-            Tap('780 650')
-            time.sleep(TRAPIDE)
-            Tap('990 860')
-            time.sleep(TRAPIDE)
-            Tap('1480 890')
-
-        #Sinon, juste relance
-        else:
-            Tap('780 650')
-            time.sleep(TRAPIDE)
-            Tap('990 860')
-            time.sleep(TRAPIDE)
-            Tap('1480 890')
-
-    else:
-        print('Infenterie deja en formation')
-        pass
 
 def Archer():
-    Tap(ImgRecherche('archerfini.png'))
+    Tap(ImgRecherche('archer.png', 0.469))
     time.sleep(TRAPIDE)
-    Tap(ImgRecherche('archer.png'))
+    Tap(ImgRecherche('archerPause.png', 0.55))
     time.sleep(TRAPIDE)
-    Tap(ImgRecherche('formation.png'))
+    Tap(ImgRecherche('formation.png', 0.99))
     time.sleep(TLENT)
     Tap(ImgRecherche('train.png'))
 
+
 def Cadeau():
-    pix = Pix(105, 208)
-    if pix <= CADEAUMAX and pix >= CADEAUMIN:
-        Tap('70 250')
-        time.sleep(TLENT)
+    pix = (105, 208)
 
-        pix = Pix(1463, 611)
-        if pix <= RECLAMERMAX and pix >= RECLAMERMIN:
-            bolReclame = True
-
-        while bolReclame:
-            Tap('1463 611')
-            time.sleep(TLENT)  
-
-            pix = Pix(1463, 611)
-            if pix > RECLAMERMAX or pix < RECLAMERMIN :
-                bolReclame = False
-
-        pix = Pix(96, 396)
-        if pix <= NOTIFMAX and pix >= NOTIFMIN:
-            Tap('170 472')
-
+ImgRecherche('train.png', 0.99)
